8acc120e2714839ffc52f828c7c97c33d31aa788
Toshiki Ishii <study.king7777@gmail.com>
2021-05-05 08:21:03 +0900

    fixurl

--- components/About.tsx
+++ components/About.tsx
@@ -26,17 +26,17 @@ export const AboutMe: React.FunctionComponent = () => {
                     <div className={styles.text}>sugar</div>
                     <div>
                         <a href="https://twitter.com/sugar235711">
-                            <img src="/twitter.png" width={50} height={50} className={styles.circle}/>
+                            <img src={`${process.env.PUBLIC_URL}/twitter.png`} width={50} height={50} className={styles.circle}/>
                         </a>            
                         <a href="https://github.com/toshikingggg">
-                        <img src="/github.png" width={50} height={50} className={styles.circle}/>
+                        <img src={`${process.env.PUBLIC_URL}/github.png`} width={50} height={50} className={styles.circle}/>
                         </a>
                     </div>
                 </div>
                 <div className={styles.profile}>
                     <Paper elevation={3} className={styles.profile_c}>{corpdatas.profile}</Paper>
                     <div>使用可能な技術</div>
-                    <img src="/p_icon.png" className={styles.img}/>
+                    <img src={`${process.env.PUBLIC_URL}/p_icon.png`} className={styles.img}/>
                 </div>            
             </div>
             <h1>Career</h1>
@@ -45,7 +45,7 @@ export const AboutMe: React.FunctionComponent = () => {
                     <>
                         <div className={styles.corp}>
                             <div>
-                                <img src= {corpdata.image} width={120} height={120} className={styles.corpicon}/>
+                                <img src= {`${process.env.PUBLIC_URL}${corpdata.image}`} width={120} height={120} className={styles.corpicon}/>
                                 <div>{corpdata.term}</div>
                             </div>
                             <Paper elevation={3} className={styles.corptext}>
