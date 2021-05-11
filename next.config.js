307e4a7b17dca852975f3b1bcdd5dd1507ab82ce
toshikingggg <69241625+toshikingggg@users.noreply.github.com>
2021-05-12 01:18:18 +0900

    Revert "自動スクロールと背景の変更"

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 module.exports = {
-  images: {
-    loader: 'imgix',
-    path: '',
-  },
-  assetPrefix: process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '',
-  trailingSlash: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      };
-    }
-
-    return config;
-  },
+    images: {
+      loader: "imgix",
+      path: "",
+    },
+    assetPrefix: process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '',
+    trailingSlash: true,
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
-import '../styles/globals.css';
-import { AppProps } from 'next/app';
-import { AnimateSharedLayout } from 'framer-motion';
+import '../styles/globals.css'
+import {AppProps} from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <AnimateSharedLayout>
-      <Component {...pageProps} />
-    </AnimateSharedLayout>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
